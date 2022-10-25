abc220e69e1f6bf47a1863c6a842d45419b3aebf
Luis Cobián <coby10@gmail.com>
2022-10-25 11:16:15 -0500

    Movimiento del jugador

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float mHor;
+    float mVer;
+    Vector3 moveDirection;
+    [SerializeField] int speed=3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mHor = Input.GetAxis("Horizontal");
+        mVer = Input.GetAxis("Vertical");
+        moveDirection.x = mHor;
+        moveDirection.y = mVer;
+        transform.position += moveDirection * Time.deltaTime * speed;
+    }
+}
